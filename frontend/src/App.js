aa8c358c218852f8886d4d8f0f4a2a199b10eabc
Maria Eduarda <anunciatomariaeduarda8@gmail.com>
2024-08-27 09:54:34 -0300

    new: design and logic alterations.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,8 @@ import './App.css';
 function App() {
   return (
     <div className="App w-screen h-screen flex justify-center items-center flex-wrap">
-      <div className="flex flex-col items-center bg-opacity-60 bg-black backdrop-blur-sm p-5 rounded-xl">
-        <h1 className="font-semibold text-white text-3xl">Chat da Duda</h1>
+      <div className="area-chat flex flex-col items-center bg-opacity-30 bg-black backdrop-blur-sm p-5 rounded-xl">
+        <h1 className="titulo-chat font-semibold text-white text-3xl">CHAT DA DUDA</h1>
         <Chat />
       </div>
     </div>
--- frontend/src/Chat.js
+++ frontend/src/Chat.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { format } from 'date-fns';
+import './App.css'
 
 const Chat = () => {
   const [messages, setMessages] = useState([]);
@@ -15,7 +16,7 @@ const Chat = () => {
       const messageObject = JSON.parse(event.data);
       const message = messageObject.message;
       const senderId = messageObject.userId;
-      const timestamp = messageObject.timestamp; // Obtém o timestamp
+      const timestamp = messageObject.timestamp;
 
       if (!userId && senderId) {
         setUserId(senderId);
@@ -44,7 +45,7 @@ const Chat = () => {
       const messageObject = {
         message: input,
         userId,
-        timestamp: new Date().toISOString(), // Adiciona o timestamp
+        timestamp: new Date().toISOString(),
       };
       socket.send(JSON.stringify(messageObject));
       setInput('');
@@ -54,13 +55,13 @@ const Chat = () => {
 
   return (
     <div className="flex flex-col bg-zinc-950 p-2 my-2 rounded-xl">
-      <div className='h-96 w-full overflow-y-auto'>
+      <div className='flex flex-col h-96 w-full overflow-y-auto'>
         {messages.map((msg, index) => {
           const formattedTime = format(new Date(msg.timestamp), 'HH:mm');
           return (
-            <div key={index} className={`flex my-2 px-3 py-2 rounded-xl w-fit max-w-96 h-fit flex-wrap items-center ${msg.senderId === userId ? 'bg-green-600 text-black self-end' : 'bg-gray-600 text-white self-start'}`}>
+            <div key={index} className={`balao-mensagem flex my-1 px-3 py-2 rounded-xl w-fit max-w-96 break-all h-fit flex-wrap items-center justify-end ${msg.senderId === userId ? 'bg-purple-600 text-white place-self-end' : 'bg-teal-500 text-black place-self-start'}`}>
               {msg.message}
-              <p className="flex text-white text-[13px] ml-2">{formattedTime}</p>
+              <p className="flex text-slate-200 text-[13px] ml-2">{formattedTime}</p>
             </div>
           );
         })}
@@ -72,9 +73,9 @@ const Chat = () => {
           placeholder='Mensagem'
           onChange={(e) => setInput(e.target.value)}
           onKeyDown={(e) => e.key === 'Enter' && sendMessage()}
-          className="w-96 h-11 px-5 py-3 border-2 border-green-600 rounded-l-full"
+          className="input-mensagem w-96 h-11 px-5 py-3 border-2 border-pink-500 text-white bg-gray-900 rounded-l-full"
         />
-        <button className="bg-green-600 hover:bg-green-700 h-11 w-16 rounded-r-full text-white flex justify-center items-center" onClick={sendMessage}>
+        <button className="bg-pink-500 hover:bg-pink-600 h-11 w-16 rounded-r-full text-white flex justify-center items-center" onClick={sendMessage}>
           <span className="material-symbols-outlined">
             send
           </span>
@@ -82,33 +83,6 @@ const Chat = () => {
       </div>
     </div>
   );
-  // return (
-  //   <div className="flex flex-col bg-zinc-950 p-2 my-2 rounded-xl">
-  //     <div className='h-96 w- overflow-y-auto'>
-  //       {messages.map((msg, index) => (
-  //         <div key={index} className='flex my-2 px-3 py-2 rounded-xl w-fit max-w-96 h-fit flex-wrap items-center justify-end break-all bg-green-600 text-black'>
-  //           {msg.message}
-  //           <p className="flex text-white text-[13px] ml-2">{formattedTime}</p>
-  //         </div>
-  //       ))}
-  //     </div>
-  //     <div className="flex items-end pt-2">
-  //       <input
-  //         type="text"
-  //         value={input}
-  //         placeholder='Mensagem'
-  //         onChange={(e) => setInput(e.target.value)}
-  //         onKeyDown={(e) => e.key === 'Enter' && sendMessage()}
-  //         className="w-96 h-11 px-5 py-3 border-2 border-green-600 rounded-l-full"
-  //       />
-  //       <button className="bg-green-600 hover:bg-green-700 h-11 w-16 rounded-r-full text-white flex justify-center items-center" onClick={sendMessage}>
-  //         <span className="material-symbols-outlined">
-  //           send
-  //         </span>
-  //       </button>
-  //     </div>
-  //   </div>
-  // );
 };
 
 export default Chat;
