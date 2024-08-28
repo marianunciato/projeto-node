8d217d652903307d4ad9f64c0902edf2db9f15f1
Maria Eduarda <anunciatomariaeduarda8@gmail.com>
2024-08-28 09:46:22 -0300

    new: login design alterations.

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import "./App.css"
 
 const Login = ({ onLogin }) => {
   const [username, setUsername] = useState('');
@@ -10,19 +11,22 @@ const Login = ({ onLogin }) => {
   };
 
   return (
-    <div className="login-container flex items-end pt-2">
-      <input
-        type="text"
-        placeholder="Oi, insira seu nome!"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        className="input-username h-11 px-5 py-3 border-2 border-pink-500 text-white bg-gray-900 bg-opacity-30 backdrop-blur-sm rounded-l-full"
-      />
-      <button onClick={handleLogin} className="btn-login w-12 bg-pink-500 hover:bg-pink-600 h-11 rounded-r-full text-white flex justify-center items-center">
-        <span className="material-symbols-outlined">
-         arrow_forward
-        </span>
-      </button>
+    <div className="login-container flex flex-col items-center">
+        <p className="text-greetings text-white mb-2 mt-5">oii, se identifica, por favor!</p>
+        <div className="flex items-end pt-2">
+            <input
+                type="text"
+                placeholder="seu nome"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="input-login h-11 px-5 py-3 border-2 border-teal-500 text-white bg-gray-900 bg-opacity-30 backdrop-blur-sm rounded-l-full"
+            />
+            <button onClick={handleLogin} className="btn-login w-12 bg-teal-500 hover:bg-teal-600 h-11 rounded-r-full text-white flex justify-center items-center">
+                <span className="material-symbols-outlined">
+                    arrow_forward
+                </span>
+            </button>
+        </div>
     </div>
   );
 };
